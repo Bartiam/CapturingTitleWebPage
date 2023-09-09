c24f287274b990fe1313797a32da18c1ba8e82bf
Andrey Golyachenko <leswar@yandex.ru>
2023-09-09 23:55:51 +0500

    Amendments.

--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,8 @@
 
 int main()
 {
-	cpr::Response r = cpr::Get(cpr::Url("http://httpbin.org/html"));
+	cpr::Response r = cpr::Get(cpr::Url("http://httpbin.org/html"), 
+		cpr::Header({{"Accept", "text/html"}}));
 	int begin = r.text.find("<h1>") + 4;
 	int end = r.text.find("</h1>");
 	std::cout << r.text.substr(begin, end - begin) << std::endl;
