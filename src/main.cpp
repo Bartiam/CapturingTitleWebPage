ea66189340a36af0f89ae13794f6277cb2307f7b
Andrey Golyachenko <leswar@yandex.ru>
2023-09-09 23:45:23 +0500

    Completion of homework is possible.

---
+++ src/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <cpr/cpr.h>
+
+int main()
+{
+	cpr::Response r = cpr::Get(cpr::Url("http://httpbin.org/html"));
+	int begin = r.text.find("<h1>") + 4;
+	int end = r.text.find("</h1>");
+	std::cout << r.text.substr(begin, end - begin) << std::endl;
+	
+
+
+	return 0;
+}
